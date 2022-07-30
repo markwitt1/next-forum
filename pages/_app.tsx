3a9653b33f5a546dea09fd562ddece315fb437ea
Mark Witt <hello@markwitt.me>
2022-07-30 18:53:24 +0200

    add MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import theme from "../src/theme";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              CODE Forum
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
